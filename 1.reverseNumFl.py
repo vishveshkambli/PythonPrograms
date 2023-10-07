5ea13a10698505ac9f1606236026711e4ef5392e
vishveshwar Kambli <vishveshkambli.864@gmail.com>
2023-10-08 00:39:17 +0530

    .

---
+++ 1.reverseNumFl.py
@@ -0,0 +1,6 @@
+num = '23597'
+rev=''
+for i in range(len(num)-1,-1,-1):
+    rev = rev + num[i]
+    print(rev)
+print('Reverse of 23597 is',rev)
--- 2.reverseNumWl.py
+++ 2.reverseNumWl.py
@@ -4,4 +4,5 @@ while num > 0:
     rem = num%10
     rev = (rev*10)+rem
     num = num//10
-print(rev)
+    print(rev)
+print('Reverse of 23597 is',rev)
