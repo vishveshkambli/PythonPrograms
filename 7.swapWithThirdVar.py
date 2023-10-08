03ad2785c3491c161b40abc71b74d061f04dc76f
vishveshwar Kambli <vishveshkambli.864@gmail.com>
2023-10-08 08:25:29 +0530

    Swap

---
+++ 7.swapWithThirdVar.py
@@ -0,0 +1,7 @@
+a=10
+b=20
+temp=a #10
+a=b #20
+b=temp #10
+print('a is swaped to ',a)
+print('b is swaped',b)
---
+++ 8.swapWithoutThirdVar.py
@@ -0,0 +1,7 @@
+a=10
+b=20
+b=a+b
+a=b-a
+b=b-a
+print('a is swapped to =',a)
+print('b is swapped to =',b)
