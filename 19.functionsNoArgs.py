3ddf80f6e8ea8b44b082aa54ecd46fea7121bf24
vishveshwar Kambli <vishveshkambli.864@gmail.com>
2023-10-13 17:49:08 +0530

    .

---
+++ 19.functionsNoArgs.py
@@ -0,0 +1,19 @@
+#Example1:
+def numbers(): 
+    num = 1,2,3,4
+    for i in num:
+        print(i)
+numbers()
+
+#Example2:
+def Goodmorning():
+    print("Good Morning")   
+Goodmorning()
+
+#Example3:
+def sum():
+    a=int(input("Please enter value of a : "))
+    b=int(input("Please enter value of b : "))
+    c=a+b
+    print(c)
+sum()
---
+++ 20.functionsArgs&Para.py
@@ -0,0 +1,13 @@
+#Example 1:
+def greet(word):
+    print(word)   
+greet("Good Morning")
+
+#Example 2:
+def operation(num1,num2):
+    print(num1+num2)
+    print(num1-num2)
+    print(num1*num2)
+    print(num1/num2)
+    print(num1%num2)
+operation(10,20)
