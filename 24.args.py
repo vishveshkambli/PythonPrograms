729083c002b0a1de52adf98a4b7f58679b3e32b9
vishveshwar Kambli <vishveshkambli.864@gmail.com>
2023-10-13 19:34:09 +0530

    .

--- 24.args.py
+++ 24.args.py
@@ -9,3 +9,10 @@ def fullname(*fname):
         
 fullname('My_firstname','My_lastname','My_Middlename')
 
+def numbers(*num): # *args can be use when you don't know how many args will be passed
+    sum=0
+    for i in num:
+        sum=sum+i
+        print(sum)
+        
+numbers(1,2,3,4,5,6,7,8,9)
